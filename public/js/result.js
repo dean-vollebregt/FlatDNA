40f0d6ae0d56f6736fac0ee0416b3a5d01dda970
Dean Vollebregt <dean.voll@gmail.com>
2017-12-25 10:18:43 +1030

    make updateColor function

--- public/js/result.js
+++ public/js/result.js
@@ -1,7 +1,14 @@
-const colourDifference = document.getElementsByClassName("difference");
+const toBeColored = document.querySelectorAll('#difference');
 
 function updateColor(){
 
+  toBeColored.forEach(function(differenceId){
+    if(differenceId.innerText.includes("-")){
+      differenceId.style.color = "green";
+    }else {
+      differenceId.style.color = "red";
+    }
+  });
 }
 
 updateColor();
